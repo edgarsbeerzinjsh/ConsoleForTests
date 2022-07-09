861171a80a92c079045dc9c2259023862e3f34be
edgarsbeerzinjsh <edgars.beerzinjsh@gmail.com>
2022-07-09 11:39:17 +0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+//HalfTriangle
+//int n = Convert.ToInt32(Console.ReadLine());
+
+//string[] narray = new string[n];
+//for (int i = 0; i < n; i++)
+//{
+//    narray[i] = new string('*', (i * 2) + 1);
+//}
+
+//for (int i = 0; i < n; i++)
+//{
+//    Console.WriteLine(narray[i]);
+//}
+
+
